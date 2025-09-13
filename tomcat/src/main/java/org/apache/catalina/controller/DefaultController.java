e0b9256bb51a0b27e821db97e0ce23d512fb022a
DongchannN <ldc991104@gmail.com>
2025-09-14 00:49:38 +0900

    refactor: DefaultController를 설정 가능한 정적 파일 서버로 개선

--- tomcat/src/main/java/org/apache/catalina/controller/DefaultController.java
+++ tomcat/src/main/java/org/apache/catalina/controller/DefaultController.java
@@ -5,22 +5,39 @@ import org.apache.coyote.http11.HttpResponse;
 import org.apache.coyote.http11.HttpStatus;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class DefaultController implements Controller {
 
+    private static final List<String> CANDIDATE_FILES = List.of("index.html", "home.html");
+    private static final String FALLBACK_MESSAGE = "Hello world!";
+
     @Override
     public HttpResponse service(final HttpRequest request) throws Exception {
-        String responseBody = "Hello world!";
+        for (String fileName : CANDIDATE_FILES) {
+            if (fileExists(fileName)) {
+                return StaticFileResponseBuilder.buildStaticFileResponse(fileName, request.version());
+            }
+        }
+        
+        return buildFallbackResponse(request.version());
+    }
+    
+    private boolean fileExists(String fileName) {
+        return this.getClass().getClassLoader().getResource("static/" + fileName) != null;
+    }
+    
+    private HttpResponse buildFallbackResponse(String httpVersion) {
         Map<String, String> headers = new HashMap<>();
         headers.put("Content-Type", "text/html;charset=utf-8");
         
         return new HttpResponse(
-                request.version(),
+                httpVersion,
                 HttpStatus.OK.getCode(),
                 HttpStatus.OK.getReasonPhrase(),
                 headers,
-                responseBody
+                FALLBACK_MESSAGE
         );
     }
 }
